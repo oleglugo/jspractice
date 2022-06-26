e664f4f2299394be14ab03b799c83ed4ed87a7c3
oleg <lugovskoy.oleg@gmail.com>
2022-06-26 11:18:57 +0300

    practice v0.1

--- src/js/script.js
+++ src/js/script.js
@@ -23,3 +23,23 @@
 
 'use strict';
 
+
+    const numberOfFilms  = +prompt("Сколько фильмов вы посмотрели?", "");
+    const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
+
+    const a = prompt ("Один из просмотренных последних фильмов", ""),
+          b = prompt ("На сколько оцените его",""),
+          c = prompt ("Один из просмотренных последних фильмов", ""),
+          d = prompt ("На сколько оцените его","");
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+
+    console.log(personalMovieDB);
